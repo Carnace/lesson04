777936975a709341d25c84a2b6ce98e2765b7c4d
kakomicli <987456NyaNya>
2021-11-15 15:26:19 +0300

    First commit

---
+++ src/test/java/tests/DragAndDropTests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void checkDragAndDropFunction(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $$("header").first().shouldHave(text("B"));
+    }
+}
---
+++ src/test/java/tests/GithabTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithabTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void checkTextOnPagesOnWiki() {
+        Configuration.headless = false;
+        // открыть страничку selenide
+        open("https://github.com/selenide/selenide");
+        // перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+        // в списке страниц (Pages) есть страница SoftAssertions
+        $(".js-wiki-more-pages-link").click();
+        // и открыть ее
+        $x("//a[.='SoftAssertions']").click();
+        //проверить, что внутри есть пример кода для JUnit5
+        $(".markdown-body").shouldHave(text("JUnit5"));
+    }
+}
